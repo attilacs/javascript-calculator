814677ed75b8a8b3401cc3b39c1ac1f02b193259
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-07 17:00:15 +0100

    Add calculator width

--- src/styles/CalculatorContainerStyled.tsx
+++ src/styles/CalculatorContainerStyled.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const CalculatorContainerStyled = styled.div`
   display: flex;
   flex-direction: column;
+  width: 152px;
 `;
 
 export default CalculatorContainerStyled;
