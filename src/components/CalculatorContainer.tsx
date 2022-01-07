da6e35061073694b4e3fcfaffe10e744471e8ff9
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-07 15:35:57 +0100

    Add numpad to calculator container

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -1,7 +1,12 @@
 import CalculatorContainerStyled from "../styles/CalculatorContainerStyled";
+import Numpad from "./Numpad";
 
 const CalculatorContainer = () => {
-  return <CalculatorContainerStyled></CalculatorContainerStyled>;
+  return (
+    <CalculatorContainerStyled>
+      <Numpad />
+    </CalculatorContainerStyled>
+  );
 };
 
 export default CalculatorContainer;
