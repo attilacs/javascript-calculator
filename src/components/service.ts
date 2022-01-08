ac5053be9cf00af0a3062d5167ead2d685948d20
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-08 16:57:26 +0100

    Create get last character function

--- src/components/service.ts
+++ src/components/service.ts
@@ -15,3 +15,4 @@ export const clearCalculator = (
 export const isNumber = (value: string) => "0" <= value && value <= "9";
 export const isOperator = (value: string) => /[x/+-]/.test(value);
 export const getFirstChar = (value: string) => value.slice(0, 1);
+export const getLastChar = (value: string) => value.slice(-1);
