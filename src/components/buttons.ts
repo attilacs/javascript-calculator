2959f4ad7d425a25264c255369ba5a832c963e9e
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-08 14:36:54 +0100

    Refactor buttons

---
+++ src/components/buttons.ts
@@ -0,0 +1,21 @@
+const buttons = [
+  { label: "=", id: "equals" },
+  { label: "0", id: "zero" },
+  { label: "1", id: "one" },
+  { label: "2", id: "two" },
+  { label: "3", id: "three" },
+  { label: "4", id: "four" },
+  { label: "5", id: "five" },
+  { label: "6", id: "six" },
+  { label: "7", id: "seven" },
+  { label: "8", id: "eight" },
+  { label: "9", id: "nine" },
+  { label: "+", id: "add" },
+  { label: "-", id: "subtract" },
+  { label: "*", id: "multiply" },
+  { label: "/", id: "divide" },
+  { label: ".", id: "decimal" },
+  { label: "AC", id: "clear" }
+];
+
+export default buttons;
