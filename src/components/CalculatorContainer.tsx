80e2f81bbc74879462556917785c42a90fef5071
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-08 17:45:26 +0100

    Refactor handle input function

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -21,37 +21,41 @@ const CalculatorContainer = () => {
     clearCalculator(setDisplayedValue, setFormula);
   };
 
+  const handleNumberInput = (value: string) => {
+    if (isLengthExceeded(displayedValue)) {
+      return;
+    }
+    if (isEvaluated) {
+      initCalculator();
+      setIsEvaluated(false);
+      setDisplayedValue(value);
+      return;
+    }
+    if (displayedValue === "0") {
+      setDisplayedValue(value);
+      return;
+    }
+    const prevValue = getLastChar(displayedValue);
+    if (isOperator(prevValue)) {
+      if (displayedValue.length === 2) {
+        const first = getFirstChar(displayedValue);
+        setFormula(formula + first);
+        setDisplayedValue("-" + value);
+        return;
+      }
+      setFormula(formula + displayedValue);
+      setDisplayedValue(value);
+      return;
+    }
+    setDisplayedValue(displayedValue + value);
+  };
+
   const handleInput = (value: string) => {
     if (value === "AC") {
       initCalculator();
     }
     if (isNumber(value)) {
-      if (isLengthExceeded(displayedValue)) {
-        return;
-      }
-      if (isEvaluated) {
-        initCalculator();
-        setIsEvaluated(false);
-        setDisplayedValue(value);
-        return;
-      }
-      if (displayedValue === "0") {
-        setDisplayedValue(value);
-        return;
-      }
-      const prevValue = getLastChar(displayedValue);
-      if (isOperator(prevValue)) {
-        if (displayedValue.length === 2) {
-          const first = getFirstChar(displayedValue);
-          setFormula(formula + first);
-          setDisplayedValue("-" + value);
-          return;
-        }
-        setFormula(formula + displayedValue);
-        setDisplayedValue(value);
-        return;
-      }
-      setDisplayedValue(displayedValue + value);
+      handleNumberInput(value);
     }
   };
 
