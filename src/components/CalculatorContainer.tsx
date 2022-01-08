4c3d12baa46db5666703231e5e1fcab9d7c1737e
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-08 17:06:57 +0100

    Add function to handle number input

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -3,7 +3,14 @@ import CalculatorContainerStyled from "../styles/CalculatorContainerStyled";
 import Display from "./Display";
 import Formula from "./Formula";
 import Numpad from "./Numpad";
-import { clearCalculator } from "./service";
+import {
+  clearCalculator,
+  getFirstChar,
+  getLastChar,
+  isLengthExceeded,
+  isNumber,
+  isOperator
+} from "./service";
 
 const CalculatorContainer = () => {
   const [displayedValue, setDisplayedValue] = useState("0");
@@ -17,6 +24,34 @@ const CalculatorContainer = () => {
   const handleInput = (value: string) => {
     if (value === "AC") {
       initCalculator();
+    }
+    if (isNumber(value)) {
+      if (isLengthExceeded(displayedValue)) {
+        return;
+      }
+      if (isEvaluated) {
+        initCalculator();
+        setIsEvaluated(false);
+        setDisplayedValue(value);
+        return;
+      }
+      if (displayedValue === "0") {
+        setDisplayedValue(value);
+        return;
+      }
+      const prevValue = getLastChar(displayedValue);
+      if (isOperator(prevValue)) {
+        if (displayedValue.length === 2) {
+          const first = getFirstChar(displayedValue);
+          setFormula(formula + first);
+          setDisplayedValue("-" + value);
+          return;
+        }
+        setFormula(formula + displayedValue);
+        setDisplayedValue(value);
+        return;
+      }
+      setDisplayedValue(displayedValue + value);
     }
   };
 
