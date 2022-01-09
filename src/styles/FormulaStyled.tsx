7304a3280839f45781248eb7d7b5b705ccc41146
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-09 13:56:54 +0100

    Modify formula font

--- src/styles/FormulaStyled.tsx
+++ src/styles/FormulaStyled.tsx
@@ -4,6 +4,8 @@ const FormulaStyled = styled.div`
   color: orange;
   display: flex;
   height: 20px;
+  font-family: "Orbitron", sans-serif;
+  font-size: 20px;
   justify-content: flex-end;
   padding-right: 5px;
 `;
