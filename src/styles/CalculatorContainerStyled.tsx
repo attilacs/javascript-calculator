0f81fd682905aae54cb27fee674f8987b95e484b
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-09 13:53:39 +0100

    Add border radius to container

--- src/styles/CalculatorContainerStyled.tsx
+++ src/styles/CalculatorContainerStyled.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const CalculatorContainerStyled = styled.div`
   background-color: black;
+  border-radius: 2px;
   display: flex;
   flex-direction: column;
   width: 152px;
