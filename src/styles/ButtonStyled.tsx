37a4d0918ab2614c852ed8fdbd5d55413604487f
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-09 13:57:54 +0100

    Modify button font

--- src/styles/ButtonStyled.tsx
+++ src/styles/ButtonStyled.tsx
@@ -4,8 +4,11 @@ const ButtonStyled = styled.div`
   align-items: center;
   background-color: gray;
   border-radius: 2px;
+  color: white;
   cursor: pointer;
   display: flex;
+  font-family: monospace;
+  font-size: 20px;
   grid-area: ${(props) => props.theme.area};
   justify-content: center;
   min-width: 25px;
