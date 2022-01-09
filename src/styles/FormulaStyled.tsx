dce50bedc0bea08a39b8c6979f199d38317c258d
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-09 13:55:47 +0100

    Modify formula spacing

--- src/styles/FormulaStyled.tsx
+++ src/styles/FormulaStyled.tsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 const FormulaStyled = styled.div`
   color: orange;
   display: flex;
+  height: 20px;
   justify-content: flex-end;
+  padding-right: 5px;
 `;
 
 export default FormulaStyled;
