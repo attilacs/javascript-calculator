77116029180364a666a5aae08bb028f583b995e1
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-09 13:56:43 +0100

    Modify display font

--- src/styles/DisplayStyled.tsx
+++ src/styles/DisplayStyled.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const DisplayStyled = styled.div`
   color: white;
   display: flex;
+  font-family: "Orbitron", sans-serif;
+  font-size: 30px;
   justify-content: flex-end;
   padding-right: 5px;
 `;
